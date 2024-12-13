bafa4f0175885624030b0f86f382970cf442c8c2
Nabin Khanal <nabinkhanal@skiff.com>
2024-12-13 18:44:11 +0545

    day6

---
+++ src/day6/solution.rs
@@ -0,0 +1,116 @@
+use std::{
+    error::Error,
+    fs::File,
+    io::{self, BufRead},
+};
+pub fn read() -> Result<(Vec<String>, (i64, i64)), Box<dyn Error>> {
+    let file = File::open("src/day6/input.txt")?;
+    let reader = io::BufReader::new(file);
+    let mut content = Vec::new();
+    let mut x: i64 = -1;
+    let mut y: i64 = -1;
+    for (_y, line) in reader.lines().enumerate() {
+        let line = line?;
+        if let Some(pos) = line.find("^") {
+            y = _y as i64;
+            x = pos as i64;
+        }
+
+        content.push(line);
+    }
+    Ok((content, (y, x)))
+}
+pub mod part1 {
+    use std::{collections, error::Error};
+
+    use super::read;
+
+    pub fn solve() -> Result<(), Box<dyn Error>> {
+        let (content, (mut y, mut x)) = read()?;
+        let mut visited = collections::HashSet::new();
+        let mut xdiff: i64 = 0;
+        let mut ydiff: i64 = -1;
+
+        let mut count: i64 = 0;
+        while x >= 0 && y >= 0 && x < content.len() as i64 && y < content.len() as i64 {
+            if (x + xdiff) >= 0
+                && (y + ydiff) >= 0
+                && (x + xdiff) < content.len() as i64
+                && (y + ydiff) < content.len() as i64
+                && content[(y + ydiff) as usize]
+                    .chars()
+                    .nth((x + xdiff) as usize)
+                    .unwrap()
+                    == '#'
+            {
+                (xdiff, ydiff) = (ydiff * -1, xdiff);
+            } else {
+                let inserted = visited.insert((y, x));
+                if inserted {
+                    count += 1;
+                }
+                x += xdiff;
+                y += ydiff;
+            }
+        }
+
+        println!("The count of visited places is: {count}");
+        Ok(())
+    }
+}
+
+pub mod part2 {
+    use super::read;
+    use std::collections;
+    use std::error::Error;
+
+    pub fn is_loop(content: &Vec<String>, o: (i64, i64), pos: (i64, i64)) -> bool {
+        let mut visited = collections::HashSet::new();
+        let mut xdiff: i64 = 0;
+        let mut ydiff: i64 = -1;
+
+        let mut y = pos.0;
+        let mut x = pos.1;
+        while x >= 0 && y >= 0 && x < content.len() as i64 && y < content.len() as i64 {
+            if ((x + xdiff) >= 0
+                && (y + ydiff) >= 0
+                && (x + xdiff) < content.len() as i64
+                && (y + ydiff) < content.len() as i64
+                && content[(y + ydiff) as usize]
+                    .chars()
+                    .nth((x + xdiff) as usize)
+                    .unwrap()
+                    == '#')
+                || (y + ydiff, x + xdiff) == o
+            {
+                (xdiff, ydiff) = (ydiff * -1, xdiff);
+            } else {
+                let new = visited.insert(((y, x), (ydiff, xdiff)));
+                if !new {
+                    return true;
+                }
+                x += xdiff;
+                y += ydiff;
+            }
+        }
+        false
+    }
+    pub fn solve() -> Result<(), Box<dyn Error>> {
+        let (content, (y, x)) = read()?;
+        let mut count = 0;
+
+        for yc in 0..content.len() {
+            for xc in 0..content.len() {
+                if content[yc].chars().nth(xc).unwrap() == '#' || (yc as i64, xc as i64) == (y, x) {
+                    continue;
+                }
+                let loops = is_loop(&content, (yc as i64, xc as i64), (y, x));
+                if loops {
+                    count += 1;
+                }
+            }
+        }
+        println!("The count of possible loops are: {count}");
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // day1::part1::solve()?;
@@ -16,6 +17,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     // day4::part1::solve()?;
     // day4::part2::solve()?;
     // day5::part1::solve()?;
-    day5::part2::solve()?;
+    // day6::part1::solve()?;
+    day6::part2::solve()?;
     Ok(())
 }
