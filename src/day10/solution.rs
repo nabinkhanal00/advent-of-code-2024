dc650a38de732a4d1227889e3a6f2a4b50d415fa
Nabin Khanal <nabinkhanal@skiff.com>
2024-12-18 21:34:09 +0545

    day10

--- src/day10/solution.rs
+++ src/day10/solution.rs
@@ -36,58 +36,6 @@ pub mod part1 {
         }
         trailheads
     }
-    pub fn count_paths() -> Result<(), Box<dyn Error>> {
-        let trail = read()?;
-        let mut m = HashMap::new();
-        let trailheads = trail_heads(&trail);
-        let length = trail.len();
-        for y in 0..length {
-            for x in 0..length {
-                let e = m.entry(trail[y][x]).or_insert(HashSet::new());
-                e.insert((y as i64, x as i64));
-            }
-        }
-        let mut dp = vec![vec![0 as i64; length]; length];
-        for key in (0..=9).rev() {
-            let value = m.get(&key).unwrap();
-            if key == 9 {
-                for v in value {
-                    dp[v.0 as usize][v.1 as usize] = 1;
-                }
-            } else {
-                let previous = m.get(&(key + 1)).unwrap();
-                for v in value {
-                    let mut count: i64 = 0;
-                    let y = v.0;
-                    let x = v.1;
-                    if previous.contains(&(y, x - 1)) {
-                        count += dp[(y) as usize][(x - 1) as usize];
-                    }
-                    if previous.contains(&(y, x + 1)) {
-                        count += dp[(y) as usize][(x + 1) as usize];
-                    }
-                    if previous.contains(&(y + 1, x)) {
-                        count += dp[(y + 1) as usize][(x) as usize];
-                    }
-                    if previous.contains(&(y - 1, x)) {
-                        count += dp[(y - 1) as usize][(x) as usize];
-                    }
-                    dp[v.0 as usize][v.1 as usize] = count;
-                }
-            }
-        }
-        let mut sum = 0;
-        for p in &dp {
-            println!("{:?}", p);
-        }
-        for head in trailheads {
-            println!("{}", dp[head.0][head.1]);
-            sum += dp[head.0][head.1];
-        }
-
-        Ok(())
-    }
-
     pub fn solve() -> Result<(), Box<dyn Error>> {
         let trail = read()?;
         let mut m = HashMap::new();
