f68f2991979473066ab240ded5f40a1062cb2544
Nabin Khanal <nabinkhanal@skiff.com>
2024-12-30 11:48:17 +0545

    renamed to 0<day>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use std::error::Error;
 
-mod day1;
+mod day01;
+mod day02;
+mod day03;
+mod day04;
+mod day05;
+mod day06;
+mod day07;
+mod day08;
+mod day09;
 mod day10;
 mod day11;
 mod day12;
@@ -10,14 +18,6 @@ mod day15;
 mod day16;
 mod day17;
 mod day18;
-mod day2;
-mod day3;
-mod day4;
-mod day5;
-mod day6;
-mod day7;
-mod day8;
-mod day9;
 
 fn main() -> Result<(), Box<dyn Error>> {
     day18::part1::solve()
